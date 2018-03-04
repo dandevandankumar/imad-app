4ef293b0330836012135e910fe60d1e1862e95c8
dande vandankumar <vandankumar4@gmail.com>
2018-03-04 14:13:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,13 +11,6 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res){
       res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-var counter=0;
-app.get('/counter',function(req,res){
-    counter=counter + 1;
-    res.send(counter.toString());
-});
-    
-});
 app.get('/article-two',function(req,res){
     res.send("article-two requested and will be served here");
 });
